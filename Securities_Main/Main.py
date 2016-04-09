a1877ef6a7bb1e532479168cd50e0afd41dba2a5
Jason Evans <evans.richard.jason@gmail.com>
2016-04-09 11:35:55 +0200

    Some minor learning changes for python

--- Securities_Main/Main.py
+++ Securities_Main/Main.py
@@ -2,4 +2,6 @@
 # Assignment by Jason R. Evans 13032608 & Vivian L. Venter 13238435
 
 class Main:
-    print 'Hello World! ', 'This is me.'
+    # Constructor for Python
+    def __init__(self):
+        print 'Hello World! ', 'This is me.\n';
