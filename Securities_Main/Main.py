c06b1f600d8160861a3df2074f34681025530912
Jason Evans <evans.richard.jason@gmail.com>
2016-04-10 13:30:01 +0200

    Some playing with the twitter data...still figuring out python object orientation for enhanced code quality

--- Securities_Main/Main.py
+++ Securities_Main/Main.py
@@ -2,6 +2,19 @@
 # Assignment by Jason R. Evans 13032608 & Vivian L. Venter 13238435
 
 import sys
+import tweepy
+import Authentication
+
+keys = Authentication.Auth()
+
+# Starting the OAuth authentication with Twitter via Tweepy
+auth = tweepy.OAuthHandler(keys.customerKey, keys.cutomerSecret)
+auth.set_access_token(keys.accessToken, keys.accessSecret)
+twitterAPI = tweepy.API(auth)
+
+publicTweets = twitterAPI.home_timeline()
+for tweet in publicTweets:
+    print tweet.text
 
 def main(args=None):
     if args is None:
