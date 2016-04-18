353a366bd6cc5411aed214e5a5d28c9155623efc
Jason Evans <evans.richard.jason@gmail.com>
2016-04-18 14:10:10 +0200

    Updating to project spec...

--- Securities_Main/Authentication.py
+++
@@ -1,8 +0,0 @@
-class Auth:
-    customerKey = '84LWQRzaQG3OJvbuVFtY1sOMd'
-    cutomerSecret = 'Vfrau3hKKpxNquSZ7x7QEJdTOA97B9ef4fUAyN8LbWs24K9O2X'
-    accessToken = '719446131428732928-lG05qNfdYU1Qe6RFbn0Qo1FEDiL59nf'
-    accessSecret = '7uda0Jf5FizPaTchx2Y54KvLSk5AwVzTIysTEFXD39nQO'
-
-    def __init__(self):
-        return None
