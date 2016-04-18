41c468ad9d815a520f39cefd23a527c71b4aabfb
Jason Evans <evans.richard.jason@gmail.com>
2016-04-18 16:57:37 +0200

    Updates on cleaning of data

--- Securities_Main/Main.py
+++ Securities_Main/Main.py
@@ -2,6 +2,7 @@
 # Assignment by Jason R. Evans 13032608 & Vivian L. Venter 13238435
 
 import sys
+import re
 import HTMLParser
 from pymongo import MongoClient
 
@@ -11,13 +12,26 @@ def cleanupContent(data):
     htmlParser = HTMLParser.HTMLParser()
 
     for tweet in allData:
+        '''
+        ### Escaping HTML Characters ###
+        '''
         content = tweet['Content']
         escapedContent = htmlParser.unescape(content)
-        print escapedContent
 
+        '''
+        ### Removing non-printable characters ###
+        '''
         stripped = lambda s: "".join(i for i in s if 31 < ord(i) < 127)
         escapedContent = stripped(escapedContent)
 
+        '''
+        ### Changing all letters to lowercase letters ###
+        '''
+        escapedContent = escapedContent.lower()
+
+        '''
+        ### Removing of URLs ###
+        '''
         while escapedContent.find("http") > -1:
             foundPosition = escapedContent.find("http")
             if foundPosition > 0:
@@ -31,8 +45,25 @@ def cleanupContent(data):
                 escapedContent = newString + escapedContent[spacePos:]
         print(escapedContent)
 
+        '''
+        ### Removing of Mentioned usernames ###
+        '''
+        listOfMentions = re.findall(r'(?<=@)\w+', escapedContent)
+        for person in listOfMentions:
+            escapedContent = escapedContent.replace('@'+person+' ', '');
+
+        '''
+        ### Removing of stopping words ###
+        '''
+        stoppingWords = ["a", "an", "is", "are", "i", "she", "he", "this", "they", "their", "that", "the", "there", "any", "to", "too"]
+        for stopWord in stoppingWords:
+            escapedContent = re.sub('\\b'+stopWord+'\\b', '', escapedContent)
+            escapedContent = escapedContent.strip()
+
+        print(escapedContent)
+
         count = count + 1
-        if count == 100:
+        if count == 10:
             break
 
 ##Main implementation
