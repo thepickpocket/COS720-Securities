58fa04f1ff813819c2b5e86280099af85d6bcdaa
Jason Evans <evans.richard.jason@gmail.com>
2016-04-25 20:35:47 +0200

    fixed bug

--- Securities_Main/Main.py
+++ Securities_Main/Main.py
@@ -62,7 +62,6 @@ while True:
     elif input == '1':
         print("Creating word cloud from twitter content data...")
         global DATA
-        cleanupContent(allData)
         WordCloud.CreateWordcloud(DATA, '../images/image.png')
         print("Word cloud created.")
     elif input == '2':
